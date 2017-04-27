5feec0c64afde57a0314af35b044e4630bab4136
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-04-27 09:08:28 +0900

    CHEVRE-37 内部関係者用の券種グループを調整

--- lib/util/ticketTypeGroup.ts
+++ lib/util/ticketTypeGroup.ts
@@ -21,11 +21,11 @@ export const TICKET_TYPE_CODE_STUDENTS_ON_THE_DAY = '03';
 /**
  * 券種(無料)
  */
-export const TICKET_TYPE_CODE_FREE = '00';
+export const TICKET_TYPE_CODE_FREE = 'FREE';
 /**
  * 券種(Not for sale)
  */
-export const TICKET_TYPE_CODE_NOT_FOR_SALE = '99';
+export const TICKET_TYPE_CODE_NOT_FOR_SALE = 'NOTFORSALE';
 
 /**
  * 内部関係者用券種グループを取得する
@@ -33,7 +33,7 @@ export const TICKET_TYPE_CODE_NOT_FOR_SALE = '99';
 export function getOne4staff() {
     return [
         {
-            code: TicketTypeGroupUtil.TICKET_TYPE_CODE_NOT_FOR_SALE,
+            _id: TicketTypeGroupUtil.TICKET_TYPE_CODE_NOT_FOR_SALE,
             name: {
                 ja: 'Not for sale',
                 en: 'Not for Sale'
@@ -41,7 +41,7 @@ export function getOne4staff() {
             charge: 0 // 料金
         },
         {
-            code: TicketTypeGroupUtil.TICKET_TYPE_CODE_FREE,
+            _id: TicketTypeGroupUtil.TICKET_TYPE_CODE_FREE,
             name: {
                 ja: '無料',
                 en: 'Free'
--- types/lib/util/ticketTypeGroup.d.ts
+++ types/lib/util/ticketTypeGroup.d.ts
@@ -13,16 +13,16 @@ export declare const TICKET_TYPE_CODE_STUDENTS_ON_THE_DAY = "03";
 /**
  * 券種(無料)
  */
-export declare const TICKET_TYPE_CODE_FREE = "00";
+export declare const TICKET_TYPE_CODE_FREE = "FREE";
 /**
  * 券種(Not for sale)
  */
-export declare const TICKET_TYPE_CODE_NOT_FOR_SALE = "99";
+export declare const TICKET_TYPE_CODE_NOT_FOR_SALE = "NOTFORSALE";
 /**
  * 内部関係者用券種グループを取得する
  */
 export declare function getOne4staff(): {
-    code: string;
+    _id: string;
     name: {
         ja: string;
         en: string;
