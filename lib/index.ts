88f13c243b094567abd33d4c029c39d9d6715193
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-05-09 13:26:19 +0900

    CHEVRE-50 オーナーユーティリティを追加

--- lib/index.ts
+++ lib/index.ts
@@ -11,6 +11,7 @@ import * as CommonUtil from './../lib/util/common';
 import * as EmailQueueUtil from './../lib/util/emailQueue';
 import * as FilmUtil from './../lib/util/film';
 import * as GMONotificationUtil from './../lib/util/gmoNotification';
+import * as OwnerUtil from './../lib/util/owner';
 import * as PerformanceUtil from './../lib/util/performance';
 import * as ReservationUtil from './../lib/util/reservation';
 import * as ScreenUtil from './../lib/util/screen';
@@ -23,6 +24,7 @@ export {
     EmailQueueUtil,
     FilmUtil,
     GMONotificationUtil,
+    OwnerUtil,
     PerformanceUtil,
     ReservationUtil,
     ScreenUtil,
