0752a26bda921824ba54cf388bbf142a96333e57
Kusunose <kusunose@motionpicture.jp>
2017-11-30 11:37:53 +0900

    TTTS-121 予約拡張情報から返金ステータス削除(パフォーマンスに移動済)

--- types/lib/model/mongoose/schemaTypes/tttsExtensionReservation.d.ts
+++ types/lib/model/mongoose/schemaTypes/tttsExtensionReservation.d.ts
@@ -3,17 +3,5 @@ declare const _default: {
         type: StringConstructor;
         required: boolean;
     };
-    refund_status: {
-        type: StringConstructor;
-        required: boolean;
-    };
-    refund_update_user: {
-        type: StringConstructor;
-        required: boolean;
-    };
-    refund_update__at: {
-        type: StringConstructor;
-        required: boolean;
-    };
 };
 export default _default;
