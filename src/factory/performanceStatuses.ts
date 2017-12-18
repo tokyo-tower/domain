2f9f979b47df2af0b2327952ea16a97ee0540237
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-12-18 13:00:17 +0000

    - パフォーマンス在庫状況をIDから取得できないバグを修正。

--- src/factory/performanceStatuses.ts
+++ src/factory/performanceStatuses.ts
@@ -10,13 +10,16 @@
 export class PerformanceStatuses {
     /**
      * パフォーマンスIDから空席ステータスを取得する
+     * @param {string} id パフォーマンスID
+     * @returns {string} 在庫状況
      */
     public getStatus(this: any, id: string): string {
-        return (this.id !== undefined) ? this[id] : '';
+        return (this[id] !== undefined) ? this[id] : '';
     }
 
     /**
      * パフォーマンスIDの空席ステータスをセットする
+     * @param {string} id パフォーマンスID
      */
     public setStatus(this: any, id: string, status: string): void {
         this[id] = status;
