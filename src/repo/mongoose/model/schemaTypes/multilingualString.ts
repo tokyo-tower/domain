79b4448916db0f5ff1dab9b3f2ad3916a2a99eab
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-12-25 08:30:02 +0000

    - 多言語スキーマを調整。

--- src/repo/mongoose/model/schemaTypes/multilingualString.ts
+++ src/repo/mongoose/model/schemaTypes/multilingualString.ts
@@ -3,24 +3,17 @@
  */
 
 export default {
-    en: {
-        type: String,
-        required: false
-    },
-    ja: {
-        type: String,
-        required: false
-    },
-    kr: {
-        type: String,
-        required: false
-    },
-    tc: {
-        type: String,
-        required: false
-    },
-    sc: {
-        type: String,
-        required: false
-    }
+    ja: { type: String, required: false },
+    en: { type: String, required: false },
+    'zh-hant': { type: String, required: false },
+    'zh-hans': { type: String, required: false },
+    ko: { type: String, required: false },
+    th: { type: String, required: false },
+    id: { type: String, required: false },
+    vi: { type: String, required: false },
+    fr: { type: String, required: false },
+    de: { type: String, required: false },
+    it: { type: String, required: false },
+    es: { type: String, required: false },
+    ru: { type: String, required: false }
 };
