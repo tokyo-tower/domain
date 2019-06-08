25c5c1ac52d54e31d6be1303002144549f5edbd6
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-06-08 02:32:29 +0000

    ### Removed - 旧在庫リポジトリを削除

--- src/repo/stock.ts
+++ src/repo/stock.ts
@@ -1,10 +1,8 @@
 import * as createDebug from 'debug';
 import * as moment from 'moment';
-import { Connection } from 'mongoose';
 import * as redis from 'redis';
 
 import * as factory from '@motionpicture/ttts-factory';
-import StockModel from '../repo/mongoose/model/stock';
 
 const debug = createDebug('chevre-domain:repo');
 
@@ -14,102 +12,6 @@ const debug = createDebug('chevre-domain:repo');
 export interface ICountResult {
     _id: string;
     count: number;
-}
-
-/**
- * 在庫リポジトリ
- */
-export class MongoRepository {
-    private readonly stockModel: typeof StockModel;
-
-    constructor(connection: Connection) {
-        this.stockModel = connection.model(StockModel.modelName);
-    }
-
-    /**
-     * まだなければ保管する
-     */
-    public async saveIfNotExists(stock: factory.stock.IStock) {
-        await this.stockModel.findOneAndUpdate(
-            {
-                _id: stock.id,
-                performance: stock.performance,
-                seat_code: stock.seat_code
-            },
-            {
-                // なければ作成
-                $setOnInsert: stock
-            },
-            {
-                upsert: true,
-                new: true
-            }
-        ).exec();
-    }
-
-    /**
-     * 在庫数をカウントする
-     */
-    public async count(params: {
-        availability: factory.itemAvailability;
-        performance: { ids: string[] };
-    }): Promise<ICountResult[]> {
-        return this.stockModel.aggregate(
-            [
-                {
-                    $match: {
-                        availability: params.availability,
-                        performance: { $in: params.performance.ids }
-                    }
-                },
-                {
-                    $group: {
-                        _id: '$performance',
-                        count: { $sum: 1 }
-                    }
-                }
-            ]
-        ).exec();
-    }
-
-    /**
-     * 在庫おさえ
-     */
-    public async lock(params: {
-        performance: string;
-        holder: string;
-    }): Promise<factory.stock.IStock | null> {
-        const doc = await this.stockModel.findOneAndUpdate(
-            {
-                performance: params.performance,
-                availability: factory.itemAvailability.InStock
-            },
-            {
-                availability: factory.itemAvailability.OutOfStock,
-                holder: params.holder
-            },
-            { new: true }
-        ).exec();
-
-        return (doc !== null) ? doc.toObject() : null;
-    }
-
-    /**
-     * 在庫解放
-     */
-    public async unlock(params: factory.reservation.event.IStock) {
-        await this.stockModel.findOneAndUpdate(
-            {
-                _id: params.id,
-                availability: params.availability_after,
-                holder: params.holder // 対象取引に保持されている
-            },
-            {
-                $set: { availability: params.availability_before },
-                $unset: { holder: 1 }
-            }
-        ).exec();
-    }
 }
 
 export interface IOffer {
