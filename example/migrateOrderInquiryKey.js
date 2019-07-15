419f52b0d98d207c584700ebb314bb68e4135de4
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-07-15 14:55:51 +0000

    ### Changed - 注文確認番号を上映日と予約番号の組み合わせに変更

--- example/migrateOrderInquiryKey.js
+++ example/migrateOrderInquiryKey.js
@@ -5,19 +5,28 @@ async function main() {
     await ttts.mongoose.connect(process.env.MONGOLAB_URI);
 
     const orderRepo = new ttts.repository.Order(ttts.mongoose.connection);
-    const orders = await orderRepo.search(
+    const cursor = await orderRepo.orderModel.find(
         {
+            orderDate: {
+                $gte: moment().add(-3, 'months').toDate(),
+                $lte: moment().toDate()
+            },
             // orderDateFrom: moment().add(-1, 'week').toDate(),
-            orderDateFrom: moment().add(-3, 'months').toDate(),
-            orderDateThrough: moment().toDate(),
-            sort: {
-                orderDate: -1,
-            }
-        }
-    );
-    console.log(orders.length, 'orders found');
+            // orderDateFrom: moment().add(-3, 'months').toDate(),
+            // orderDateThrough: moment().toDate(),
+            // sort: {
+            //     orderDate: -1,
+            // }
+        },
+        { orderNumber: 1, orderInquiryKey: 1 }
+    ).sort({ orderDate: -1 }).cursor();
+    console.log('orders found');
+    // return;
 
-    for (const order of orders) {
+    let i = 0;
+    await cursor.eachAsync(async (doc) => {
+        i += 1;
+        const order = doc.toObject();
         const confirmationNumber = `${order.orderInquiryKey.performanceDay}${order.orderInquiryKey.paymentNo}`;
         await orderRepo.orderModel.findOneAndUpdate(
             { orderNumber: order.orderNumber },
@@ -25,8 +34,22 @@ async function main() {
                 confirmationNumber: confirmationNumber
             }
         ).exec();
-        console.log('migrated', order.orderNumber)
-    }
+        console.log('migrated', order.orderNumber, i);
+    });
+
+    // for (const order of orders) {
+    //     const confirmationNumber = `${order.orderInquiryKey.performanceDay}${order.orderInquiryKey.paymentNo}`;
+    //     await orderRepo.orderModel.findOneAndUpdate(
+    //         { orderNumber: order.orderNumber },
+    //         {
+    //             confirmationNumber: confirmationNumber
+    //         }
+    //     ).exec();
+    //     console.log('migrated', order.orderNumber)
+    // }
+
+    console.log(i, 'orders migrated');
+    // console.log(orders.length, 'orders migrated');
 }
 
 main()
