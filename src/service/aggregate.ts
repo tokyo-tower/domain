8918ab6220c17e72fd852440ea6c0a6fae17235c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-08-05 01:02:56 +0000

    ### Changed - パフォーマンス残席数に、Chevreイベントのオファーごとの集計情報を利用するように調整

--- src/service/aggregate.ts
+++ src/service/aggregate.ts
@@ -24,7 +24,7 @@ export enum TicketTypeCategory {
     Wheelchair = 'Wheelchair'
 }
 
-const WHEEL_CHAIR_NUM_ADDITIONAL_STOCKS = 6;
+// const WHEEL_CHAIR_NUM_ADDITIONAL_STOCKS = 6;
 
 const cinerinoAuthClient = new cinerinoapi.auth.ClientCredentials({
     domain: credentials.cinerino.authorizeServerDomain,
@@ -178,9 +178,9 @@ function aggregateByEvent(params: {
                 onlineSalesStatus: (performance.ttts_extension !== undefined)
                     ? performance.ttts_extension.online_sales_status
                     : factory.performance.OnlineSalesStatus.Normal,
-                maximumAttendeeCapacity: maximumAttendeeCapacity,
-                remainingAttendeeCapacity: remainingAttendeeCapacity,
-                remainingAttendeeCapacityForWheelchair: remainingAttendeeCapacityForWheelchair,
+                maximumAttendeeCapacity: <number>maximumAttendeeCapacity,
+                remainingAttendeeCapacity: <number>remainingAttendeeCapacity,
+                remainingAttendeeCapacityForWheelchair: <number>remainingAttendeeCapacityForWheelchair,
                 reservationCount: reservations.length,
                 checkinCount: checkinCountAggregation.checkinCount,
                 reservationCountsByTicketType: offersAggregation.map((offer) => {
@@ -217,14 +217,20 @@ function saveAggregation2performance(params: factory.performance.IPerformanceWit
                 updated_at: new Date(), // $setオブジェクトが空だとMongoエラーになるので
                 evServiceStatus: params.evServiceStatus,
                 onlineSalesStatus: params.onlineSalesStatus,
-                maximumAttendeeCapacity: params.maximumAttendeeCapacity,
-                remainingAttendeeCapacity: params.remainingAttendeeCapacity,
-                remainingAttendeeCapacityForWheelchair: params.remainingAttendeeCapacityForWheelchair,
                 reservationCount: params.reservationCount,
                 checkinCount: params.checkinCount,
                 reservationCountsByTicketType: params.reservationCountsByTicketType,
                 checkinCountsByWhere: params.checkinCountsByWhere,
                 tourNumber: (<any>params).tourNumber,
+                ...(typeof params.maximumAttendeeCapacity === 'number')
+                    ? { maximumAttendeeCapacity: params.maximumAttendeeCapacity }
+                    : undefined,
+                ...(typeof params.remainingAttendeeCapacity === 'number')
+                    ? { remainingAttendeeCapacity: params.remainingAttendeeCapacity }
+                    : undefined,
+                ...(typeof params.remainingAttendeeCapacityForWheelchair === 'number')
+                    ? { remainingAttendeeCapacityForWheelchair: params.remainingAttendeeCapacityForWheelchair }
+                    : undefined,
                 ...(Array.isArray(params.offers)) ? { offers: params.offers } : undefined
             },
             $unset: {
@@ -245,66 +251,76 @@ function aggregateRemainingAttendeeCapacity(params: {
     performance: { id: string };
 }) {
     return async () => {
-        const event = await eventService.findById({ id: params.performance.id });
-        const seller = event.offers?.seller;
-        if (seller === undefined) {
-            throw new factory.errors.NotFound('Event Seller');
-        }
+        const event = await eventService.findById<cinerinoapi.factory.chevre.eventType.ScreeningEvent>({ id: params.performance.id });
 
-        const screeningRoomSectionOffers = await eventService.searchOffers({ event: { id: event.id } });
-        const ticketOffers = await eventService.searchTicketOffers({
-            event: { id: event.id },
-            seller: {
-                typeOf: <cinerinoapi.factory.organizationType>seller.typeOf,
-                id: <string>seller.id
-            },
-            store: { id: credentials.cinerino.clientId }
-        });
-
-        const sectionOffer = screeningRoomSectionOffers[0];
-
-        // 一般座席
-        const normalSeats = sectionOffer.containsPlace.filter(
-            (s) => (typeof s.seatingType === 'string' && s.seatingType === SeatingType.Normal)
-                || (Array.isArray(s.seatingType) && s.seatingType.includes(SeatingType.Normal))
-        );
-        // 全車椅子座席
-        const wheelChairSeats = sectionOffer.containsPlace.filter(
-            (s) => (typeof s.seatingType === 'string' && s.seatingType === SeatingType.Wheelchair)
-                || (Array.isArray(s.seatingType) && s.seatingType.includes(SeatingType.Wheelchair))
-        );
-
-        // maximumAttendeeCapacityは一般座席数
-        const maximumAttendeeCapacity = normalSeats.length;
-        let remainingAttendeeCapacity = maximumAttendeeCapacity;
-        let remainingAttendeeCapacityForWheelchair = wheelChairSeats.length;
-
-        const availableSeatNumbers = sectionOffer.containsPlace.filter((s) => {
-            return Array.isArray(s.offers)
-                && s.offers.length > 0
-                && s.offers[0]?.availability === cinerinoapi.factory.chevre.itemAvailability.InStock;
-        }).map((s) => s.branchCode);
-        debug('availableSeatNumbers:', availableSeatNumbers.length);
-
-        remainingAttendeeCapacity = normalSeats.filter((s) => availableSeatNumbers.includes(s.branchCode)).length;
-        remainingAttendeeCapacityForWheelchair = wheelChairSeats.filter((s) => availableSeatNumbers.includes(s.branchCode)).length;
-
-        // 車椅子確保分が一般座席になければ車椅子は0(同伴者考慮)
-        if (remainingAttendeeCapacity < WHEEL_CHAIR_NUM_ADDITIONAL_STOCKS + 1) {
-            remainingAttendeeCapacityForWheelchair = 0;
-        }
-
-        // 流入制限保持者がいれば車椅子在庫は0
-        const wheelChairOffer = ticketOffers.find((o) => {
-            const ticketTypeCategory = o.additionalProperty?.find((p) => p.name === 'category')?.value;
-
-            return ticketTypeCategory === TicketTypeCategory.Wheelchair;
-        });
-        if (wheelChairOffer?.availability === factory.chevre.itemAvailability.OutOfStock) {
-            remainingAttendeeCapacityForWheelchair = 0;
-        }
+        // Chevreのオファーごと集計を利用する場合は↓
+        const aggregateOffer = event.aggregateOffer;
+        const maximumAttendeeCapacity = aggregateOffer?.offers?.find((o) => o.identifier === '001')?.maximumAttendeeCapacity;
+        const remainingAttendeeCapacity = aggregateOffer?.offers?.find((o) => o.identifier === '001')?.remainingAttendeeCapacity;
+        const remainingAttendeeCapacityForWheelchair =
+            aggregateOffer?.offers?.find((o) => o.identifier === '004')?.remainingAttendeeCapacity;
 
         return { maximumAttendeeCapacity, remainingAttendeeCapacity, remainingAttendeeCapacityForWheelchair };
+
+        // const seller = event.offers?.seller;
+        // if (seller === undefined) {
+        //     throw new factory.errors.NotFound('Event Seller');
+        // }
+
+        // const screeningRoomSectionOffers = await eventService.searchOffers({ event: { id: event.id } });
+        // const ticketOffers = await eventService.searchTicketOffers({
+        //     event: { id: event.id },
+        //     seller: {
+        //         typeOf: <cinerinoapi.factory.organizationType>seller.typeOf,
+        //         id: <string>seller.id
+        //     },
+        //     store: { id: credentials.cinerino.clientId }
+        // });
+
+        // const sectionOffer = screeningRoomSectionOffers[0];
+
+        // // 一般座席
+        // const normalSeats = sectionOffer.containsPlace.filter(
+        //     (s) => (typeof s.seatingType === 'string' && s.seatingType === SeatingType.Normal)
+        //         || (Array.isArray(s.seatingType) && s.seatingType.includes(SeatingType.Normal))
+        // );
+        // // 全車椅子座席
+        // const wheelChairSeats = sectionOffer.containsPlace.filter(
+        //     (s) => (typeof s.seatingType === 'string' && s.seatingType === SeatingType.Wheelchair)
+        //         || (Array.isArray(s.seatingType) && s.seatingType.includes(SeatingType.Wheelchair))
+        // );
+
+        // // maximumAttendeeCapacityは一般座席数
+        // const maximumAttendeeCapacity = normalSeats.length;
+        // let remainingAttendeeCapacity = maximumAttendeeCapacity;
+        // let remainingAttendeeCapacityForWheelchair = wheelChairSeats.length;
+
+        // const availableSeatNumbers = sectionOffer.containsPlace.filter((s) => {
+        //     return Array.isArray(s.offers)
+        //         && s.offers.length > 0
+        //         && s.offers[0]?.availability === cinerinoapi.factory.chevre.itemAvailability.InStock;
+        // }).map((s) => s.branchCode);
+        // debug('availableSeatNumbers:', availableSeatNumbers.length);
+
+        // remainingAttendeeCapacity = normalSeats.filter((s) => availableSeatNumbers.includes(s.branchCode)).length;
+        // remainingAttendeeCapacityForWheelchair = wheelChairSeats.filter((s) => availableSeatNumbers.includes(s.branchCode)).length;
+
+        // // 車椅子確保分が一般座席になければ車椅子は0(同伴者考慮)
+        // if (remainingAttendeeCapacity < WHEEL_CHAIR_NUM_ADDITIONAL_STOCKS + 1) {
+        //     remainingAttendeeCapacityForWheelchair = 0;
+        // }
+
+        // // 流入制限保持者がいれば車椅子在庫は0
+        // const wheelChairOffer = ticketOffers.find((o) => {
+        //     const ticketTypeCategory = o.additionalProperty?.find((p) => p.name === 'category')?.value;
+
+        //     return ticketTypeCategory === TicketTypeCategory.Wheelchair;
+        // });
+        // if (wheelChairOffer?.availability === factory.chevre.itemAvailability.OutOfStock) {
+        //     remainingAttendeeCapacityForWheelchair = 0;
+        // }
+
+        // return { maximumAttendeeCapacity, remainingAttendeeCapacity, remainingAttendeeCapacityForWheelchair };
     };
 }
 
