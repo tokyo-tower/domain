1d6d72a97be39769be5e52a746493a58c5da6915
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-01-05 05:17:10 +0000

    - イベント予約集計タスクを削除 - 注文取引レポート作成タスクを削除 - 注文返品取引レポート作成タスクを削除 - Cinerinoイベントインポートタスクを削除

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 /**
  * index module
  */
-import * as AggregateService from './service/aggregate';
 import * as NotificationService from './service/notification';
 import * as OrderReportService from './service/report/order';
 import * as TaskService from './service/task';
@@ -12,7 +11,6 @@ import * as repository from './repository';
 
 export namespace service {
     export import notification = NotificationService;
-    export import aggregate = AggregateService;
     export namespace report {
         export import order = OrderReportService;
     }
