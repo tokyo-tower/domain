a5a15e5b1ec1eb0a9d0f594077bff218278b2a27
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-01-08 04:43:57 +0000

    - タスクリポジトリを削除

--- src/repository.ts
+++ src/repository.ts
@@ -5,7 +5,6 @@
 import { MongoRepository as PerformanceRepo } from './repo/performance';
 import { MongoRepository as ReportRepo } from './repo/report';
 import { MongoRepository as ReservationRepo } from './repo/reservation';
-import { MongoRepository as TaskRepo } from './repo/task';
 
 /**
  * パフォーマンスリポジトリ
@@ -21,8 +20,3 @@ export class Report extends ReportRepo { }
  * 予約リポジトリ
  */
 export class Reservation extends ReservationRepo { }
-
-/**
- * タスクリポジトリ
- */
-export class Task extends TaskRepo { }
